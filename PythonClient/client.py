7754f3b2d255a3f15e80bf241302146f07065eb5
amir sherafatian <sherafatian.amirabbas@gmail.com>
2021-12-19 18:54:58 +0200

    assets created; bin extracted

--- PythonClient/client.py
+++ PythonClient/client.py
@@ -2,19 +2,26 @@ import ctypes
 import pathlib
 
 rootpath  = pathlib.Path('.').resolve()
+
+# needed for debugging
+# rootpath = str(rootpath) + '/pythonclient'
+
 library = ctypes.cdll.LoadLibrary(f'{rootpath}/bin/trie.so')
 
+
+Initialize_Trie = library.InitializeTrie
+Add_Keyword = library.AddKeyword
+Match_Letter = library.MatchLetter
+
+
 def TrieInitialize():
-    Initialize_Trie = library.InitializeTrie
     Initialize_Trie()
 
 def Add(keyword):
-    Add_Keyword = library.AddKeyword
     Add_Keyword.argtypes = [ctypes.c_char_p]
     Add_Keyword(keyword.encode('utf-8'))
 
 def Match(letter):
-    Match_Letter = library.MatchLetter
     Match_Letter.argtypes = [ctypes.c_char_p]
     Match_Letter.restype = ctypes.c_void_p
     ptr = Match_Letter(letter.encode('utf-8'))
